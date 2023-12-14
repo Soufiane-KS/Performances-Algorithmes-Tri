9aeb8b768b6aa1927616760a5e0106b028c65761
Soufiane <Soufianekousta1@gmail.com>
2023-12-14 19:58:11 +0100

    modification test

--- main.c
+++ main.c
@@ -7,15 +7,16 @@
 #define MAX_SIZE 100000
 
 
+
 int main(int argc , char* argv[]){
 
     int *t;
     int i,choix,moyenneDeDonnees,conteur=0;
-    double tempsMoyenneInsertion,tempsMoyenneBulle,tempsMoyenneSelection;
+    double tempsMoyenneInsertion,tempsMoyenneBulle,tempsMoyenneSelection; //utilisé pour le lissage
   FILE* f = fopen("data.txt","w");
     if (f!=NULL)
-    { 
-            fprintf(f,"0\t0\t0\t0\t\n");   
+    {
+            fprintf(f,"0\t0\t0\t0\t\n");
     printf("\n|-----------------Comparaison du performance des algorithmes de tri-----------------|\nSelectionner l'un des tests a est effectue.(entre 1 et 4)\n\t1-test aleatoire.\n\t2-test favorable au algorithme insertion.\n\t3-test favorable au algorithme bulles.\n\t4-test favorable au algorithme selection.\n\tVotre choix:");
     scanf("%d",&choix);
     if(choix == 1|| choix == 2 || choix == 3 || choix == 4){
@@ -38,7 +39,7 @@ int main(int argc , char* argv[]){
             tempsMoyenneSelection+=time3;
             moyenneDeDonnees+=i;
             if(conteur%3 == 0){
-            fprintf(f,"%d\t%lf\t%lf\t%lf\t\n",moyenneDeDonnees/3,tempsMoyenneInsertion/3,tempsMoyenneBulle/3,tempsMoyenneSelection/3);   
+            fprintf(f,"%d\t%lf\t%lf\t%lf\t\n",moyenneDeDonnees/3,tempsMoyenneInsertion/3,tempsMoyenneBulle/3,tempsMoyenneSelection/3);
             printf("%d\t%lf\t%lf\t%lf\t\n",moyenneDeDonnees/3,tempsMoyenneInsertion/3,tempsMoyenneBulle/3,tempsMoyenneSelection/3);
             tempsMoyenneInsertion =0;
             tempsMoyenneBulle=0;
@@ -51,7 +52,7 @@ int main(int argc , char* argv[]){
         }
         fclose(f);
         tracer_courbe();
-    
+
     }else
         printf("ouverture du fichier est imposiible !");
 
